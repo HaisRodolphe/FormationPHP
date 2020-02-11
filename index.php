cbc01c5bac46bc672bd1251eb02ef92e4c2ab3af
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-02-11 08:49:11 +0100

    Creation Branche envoi email

--- index.php
+++ index.php
@@ -1,52 +1,84 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-    <h2>Affichage de texte avec PHP</h2>
-        
-        <p>
-            Cette ligne a été écrite entièrement en HTML.<br />
-            <?php echo "Celle-ci a été écrite entièrement en PHP."; ?>
-        </p>
+<?php 
+if(isset($_POST['email']) && $_POST['email']!="") {
+	$destinataire = $_POST['email'];
+	
+	if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $destinataire)) {
+		$sautLigne = "\r\n";
+	}
+	else {
+		$sautLigne = "\n";
+	}
 
-        <!--Les variables-->
-        <?php
-        $age_du_visiteur = 17;
-        echo $age_du_visiteur; //Afficher les variables.
-        ?>
+	$separateur = md5(uniqid(microtime(), TRUE));
+		
+	$header = "From: \"Mon site web\"<monsite@adresse.fr>".$sautLigne;
+	$header .= "Mime-Version: 1.0".$sautLigne;
+	$header .= "Content-Type: multipart/mixed;boundary=".$separateur.$sautLigne;
+	$header .= $sautLigne;
 
-        <?php
-        $age_du_visiteur = 17;
-        echo 'Le visiteur a ' . $age_du_visiteur . ' ans';//Afficher les variables la bonne écriture. 
-        ?>
-        
-        <!--Les calcules-->
-        <?php
-        $nombre = 2 + 4; // $nombre prend la valeur 6
-        $nombre = 5 - 1; // $nombre prend la valeur 4
-        $nombre = 3 * 5; // $nombre prend la valeur 15
-        $nombre = 10 / 2; // $nombre prend la valeur 5
+	$objet = "Inscription à la newsletter";
 
-        // Allez on rajoute un peu de difficulté
-        $nombre = 3 * 5 + 1; // $nombre prend la valeur 16
-        $nombre = (1 + 2) * 2; // $nombre prend la valeur 6
-        ?>
-        
-        <?php
-        $nombre = 10;
-        $resultat = ($nombre + 5) * $nombre; // $resultat prend la valeur 150
-        ?>
+	$msg = "--".$separateur.$sautLigne;
+	$msg .= "Content-type:text/plain;charset=utf-8".$sautLigne;
+	$msg .= "Content-transfer-encoding:8bit".$sautLigne;
+	$msg .= "Voici un message avec une pièce jointe.".$sautLigne;
+ 
+	// Pièce jointe
+	$fichier = "image.jpg";
+	if (file_exists($fichier)) {
+		$typeFichier = filetype($fichier);
+		$tailleFichier = filesize($fichier);
+	 
+		$handle = fopen($fichier, "r") or die("Impossible d'ouvrir le fichier ".$fichier);
+		$contenu = fread($handle, $tailleFichier);
+		$contenu = chunk_split(base64_encode($contenu));
+		$f = fclose($handle);
+	 
+		$msg .= "--".$separateur.$sautLigne;
+		$msg .= "Content-type:".$typeFichier.";name=".$fichier.$sautLigne;
+		$msg .= "Content-transfer-encoding:base64".$sautLigne;
+		$msg .= $contenu.$sautLigne;
+	}
+	
+	// 2ème pièce jointe
+	$fichier = "catalogue.pdf";
+	if (file_exists($fichier)) {
+		$typeFichier = filetype($fichier);
+		$tailleFichier = filesize($fichier);
+	 
+		$handle = fopen($fichier, "r") or die("Impossible d'ouvrir le fichier ".$fichier);
+		$contenu = fread($handle, $tailleFichier);
+		$contenu = chunk_split(base64_encode($contenu));
+		$f = fclose($handle);
+	 
+		$msg .= "--".$separateur.$sautLigne;
+		$msg .= "Content-type:".$typeFichier.";name=".$fichier.$sautLigne;
+		$msg .= "Content-transfer-encoding:base64".$sautLigne;
+		$msg .= $contenu.$sautLigne;
+	}
+ 
+	$msg .= "--".$separateur.$sautLigne;
+	
+	if(filter_var($destinataire, FILTER_VALIDATE_EMAIL)) {
+		echo "Le mail est correct";
+		mail($destinataire, $objet, $msg, $header);
+		echo "<br/>Newsletter envoyée à ".$_POST['email'];
+	}
+	else {
+		echo "Le mail est incorrect";
+	}
+	
+}
+else {
+	?>
+	<h1>Inscrivez-vous à notre newsletter</h1>
+	<form method="post" action="index.php">
+		<label for="email">Votre e-mail</label> : <input type="text" name="email" placeholder="Votre e-mail" />
+		<input type="submit" value="Valider" />
+	</form> 
+	<?php
+}
+?>
 
-        <script src="" async defer></script>
-    </body>
-</html>
+
+

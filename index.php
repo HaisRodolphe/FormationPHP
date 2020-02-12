26a7dcb03d8201505e11d32232be48b4607fbbb3
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-02-12 12:45:56 +0100

    Creation branche"Les boucles" Instruction "whild"

--- index.php
+++ index.php
@@ -1,82 +1,26 @@
-<?php 
-if(isset($_POST['email']) && $_POST['email']!="") {
-	$destinataire = $_POST['email'];
-	
-	if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $destinataire)) {
-		$sautLigne = "\r\n";
-	}
-	else {
-		$sautLigne = "\n";
-	}
+<!--Les Boucles-->
 
-	$separateur = md5(uniqid(microtime(), TRUE));
-		
-	$header = "From: \"Mon site web\"<monsite@adresse.fr>".$sautLigne;
-	$header .= "Mime-Version: 1.0".$sautLigne;
-	$header .= "Content-Type: multipart/mixed;boundary=".$separateur.$sautLigne;
-	$header .= $sautLigne;
-
-	$objet = "Inscription à la newsletter";
-
-	$msg = "--".$separateur.$sautLigne;
-	$msg .= "Content-type:text/plain;charset=utf-8".$sautLigne;
-	$msg .= "Content-transfer-encoding:8bit".$sautLigne;
-	$msg .= "Voici un message avec une pièce jointe.".$sautLigne;
- 
-	// Pièce jointe
-	$fichier = "image.jpg";
-	if (file_exists($fichier)) {
-		$typeFichier = filetype($fichier);
-		$tailleFichier = filesize($fichier);
-	 
-		$handle = fopen($fichier, "r") or die("Impossible d'ouvrir le fichier ".$fichier);
-		$contenu = fread($handle, $tailleFichier);
-		$contenu = chunk_split(base64_encode($contenu));
-		$f = fclose($handle);
-	 
-		$msg .= "--".$separateur.$sautLigne;
-		$msg .= "Content-type:".$typeFichier.";name=".$fichier.$sautLigne;
-		$msg .= "Content-transfer-encoding:base64".$sautLigne;
-		$msg .= $contenu.$sautLigne;
-	}
-	
-	// 2ème pièce jointe
-	$fichier = "catalogue.pdf";
-	if (file_exists($fichier)) {
-		$typeFichier = filetype($fichier);
-		$tailleFichier = filesize($fichier);
-	 
-		$handle = fopen($fichier, "r") or die("Impossible d'ouvrir le fichier ".$fichier);
-		$contenu = fread($handle, $tailleFichier);
-		$contenu = chunk_split(base64_encode($contenu));
-		$f = fclose($handle);
-	 
-		$msg .= "--".$separateur.$sautLigne;
-		$msg .= "Content-type:".$typeFichier.";name=".$fichier.$sautLigne;
-		$msg .= "Content-transfer-encoding:base64".$sautLigne;
-		$msg .= $contenu.$sautLigne;
-	}
- 
-	$msg .= "--".$separateur.$sautLigne;
-	
-	if(filter_var($destinataire, FILTER_VALIDATE_EMAIL)) {
-		echo "Le mail est correct";
-		mail($destinataire, $objet, $msg, $header);
-		echo "<br/>Newsletter envoyée à ".$_POST['email'];
-	}
-	else {
-		echo "Le mail est incorrect";
-	}
-	
+<!--Les Boucles simple: while-->
+<!--whilepeut se traduire par « tant que ». 
+Ici, on demande à PHP : TANT QUE$continuer_boucleest vrai, 
+exécuter ces instructions.-->
+<?php
+while ($continuer_boucle == true)
+{
+    // instructions à exécuter dans la boucle
 }
-else {
-	?>
-	<h1>Inscrivez-vous à notre newsletter</h1>
-	<form method="post" action="index.php">
-		<label for="email">Votre e-mail</label> : <input type="text" name="email" placeholder="Votre e-mail" />
-		<input type="submit" value="Valider" />
-	</form> 
-	<?php
+?>
+
+<!-- vous avez été punis et que vous devez recopier 100 fois 
+« Je ne dois pas regarder les mouches voler quand j'apprends le PHP. ».-->
+<!--La boucle pose la condition : TANT QUE$nombre_de_lignesest inférieur ou égal à 100.-->
+<?php
+$nombre_de_lignes = 1;
+
+while ($nombre_de_lignes <= 100)
+{
+    echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP.<br />';
+    $nombre_de_lignes++; // $nombre_de_lignes = $nombre_de_lignes + 1
 }
 ?>
 
